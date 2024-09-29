13aa212949e937da04e110c1b5bee405a327d08e
Krestom <ilya.laptev2003@mail.ru>
2024-09-29 17:04:37 +0000

    Базовая модель catboost с кодированием One-Hot и CatBoost

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,7 +1,6 @@
 # scripts/fit.py
 
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from category_encoders import CatBoostEncoder
@@ -23,19 +22,24 @@ def fit_model():
 
     # Определите категориальные и числовые признаки
     cat_features = data.select_dtypes(include='object')
-    num_features = data.select_dtypes(include=['float'])
+    potential_binary_features = cat_features.nunique() == 2
+
+    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
+    other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
+    num_features = data.select_dtypes(['float'])
 
     # Создайте трансформеры и модель
     preprocessor = ColumnTransformer(
         [
-            ('cat', OneHotEncoder(drop=params['one_hot_drop']), cat_features.columns.tolist()),
+            ('binary', OneHotEncoder(drop=params['one_hot_drop']), binary_cat_features.columns.tolist()),
+            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
             ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    model = LogisticRegression(C=params['C'], penalty=params['penalty'])
+    model = CatBoostClassifier(auto_class_weights=params['auto_class_weights'], verbose=False)
 
     # Объедините всё в пайплайн
     pipeline = Pipeline(
@@ -46,7 +50,7 @@ def fit_model():
     )
 
     # Обучите модель
-    pipeline.fit(data.drop(columns=[params['target_col']]), data[params['target_col']])
+    pipeline.fit(data, data[params['target_col']])
 
     # Сохраните обученную модель
     os.makedirs('models', exist_ok=True)  # Убедитесь, что папка существует
@@ -54,5 +58,5 @@ def fit_model():
     joblib.dump(pipeline, model_path)
 
 
-if __name__ == '__main__':
+if name == 'main':
     fit_model()
